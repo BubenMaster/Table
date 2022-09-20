a3b5c6a49dc7564462b829d9070da459ae597c48
YojZFK <vzikich@rambler.ru>
2022-09-20 14:30:16 +0300

    Initial empty commit

---
+++ src/test/java/com/cwt/task/table/TableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwt.task.table;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
