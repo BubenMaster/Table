be237c9c85e0caa8904ce60316fc3fabd471c66c
YojZFK <vzikich@rambler.ru>
2022-09-21 14:26:44 +0300

    Added SQLite Database table.db

--- src/main/java/com/cwt/task/table/TableApplication.java
+++ src/main/java/com/cwt/task/table/TableApplication.java
@@ -2,12 +2,14 @@ package com.cwt.task.table;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.jooq.*;
 
 @SpringBootApplication
 public class TableApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TableApplication.class, args);
+
 	}
 
 }
