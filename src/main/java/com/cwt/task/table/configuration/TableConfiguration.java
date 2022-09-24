360832d64f9bbb229dea0650125172652778d5e2
YojZFK <vzikich@rambler.ru>
2022-09-24 21:55:17 +0300

    Moved table.db to the root

--- src/main/java/com/cwt/task/table/configuration/TableConfiguration.java
+++ src/main/java/com/cwt/task/table/configuration/TableConfiguration.java
@@ -75,7 +75,7 @@ public class TableConfiguration {
     public DataSource dataSource(){
         HikariConfig config = new HikariConfig();
         config.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
-        config.setJdbcUrl("jdbc:sqlite:database\\table.db"); // TODO: 22.09.2022 Implement reading from properties
+        config.setJdbcUrl(env.getProperty("spring.datasource.url")); // TODO: 22.09.2022 Implement reading from properties
         HikariDataSource dSource = new HikariDataSource(config);
 
         return dSource;
